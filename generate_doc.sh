3d952833a52010e9170ae3a0f5fe410d66e7ab75
Stepan Tikunov <stepan.tikunov@niuitmo.ru>
2022-04-26 05:38:02 +0300

    Move doc/ to docs/

--- generate_doc.sh
+++ generate_doc.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 
 cd "$(dirname "$0")" &&
-javadoc -sourcepath src -d doc -cp $(cat .classpath) -subpackages edu.ifmo.tikunov.lab4 ;
+javadoc -sourcepath src -d docs -cp $(cat .classpath) -subpackages edu.ifmo.tikunov.lab4 ;
